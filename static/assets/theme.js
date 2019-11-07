aa94ebda284e18529b063e3ef94d45d79d5749b2
Yash Raj Bharti <yashrajbharti.met18@itbhu.ac.in>
2019-11-08 01:01:08 +0530

    local storage in theme.js

--- static/assets/theme.js
+++ static/assets/theme.js
@@ -1,31 +1,42 @@
-$(document).ready(function(){
-	var d = new Date();
-	var n = d.getHours();
-	if (n > 19 || n < 6){
-	  // If time is after 7PM or before 6AM, apply night theme to ‘body’
-    document.documentElement.setAttribute('data-theme', 'dark')
-    $('#switch').click();
-
-  }
-	else
-	  // Else use ‘day’ theme
-
-    document.documentElement.setAttribute('data-theme', 'light')
-
-});
+function toBeExecutedOnFirstLoad(){
+	$(document).ready(function(){
+		var d = new Date();
+		var n = d.getHours();
+		if (n > 6 && n < 19){
+		  // If time is after 6AM or before 7PM, apply day theme to ‘body’
+			document.documentElement.setAttribute('data-theme', 'light')
+			window.localStorage.setItem('data-theme', 'light');
+	    $('#switch').click();
+	  }
+		else{
+		  // Else use ‘night’ theme
+			document.documentElement.setAttribute('data-theme', 'dark')
+			window.localStorage.setItem('data-theme', 'dark');
+			}
+	});
+}
+if(localStorage.getItem('first') === null){
+  toBeExecutedOnFirstLoad();
+  localStorage.setItem('first','nope!');
+}
 
 var checkbox = document.querySelector('input[name=theme]');
 
-checkbox.addEventListener('change', function() {
-    if(this.checked) {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'dark')
-    } else {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'light')
-    }
-})
+var theme = window.localStorage.getItem('data-theme');
+if(theme) document.documentElement.setAttribute('data-theme', theme);
+checkbox.checked = theme == 'dark' ? true : false;
 
+checkbox.addEventListener('change', function () {
+  if(this.checked){
+		trans()
+    document.documentElement.setAttribute('data-theme', 'dark');
+    window.localStorage.setItem('data-theme', 'dark');
+  } else {
+		trans()
+    document.documentElement.setAttribute('data-theme', 'light');
+    window.localStorage.setItem('data-theme', 'light');
+}
+});
 let trans = () => {
     document.documentElement.classList.add('transition');
     window.setTimeout(() => {
