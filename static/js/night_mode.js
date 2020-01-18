e5d609b701ea963a5b91afb23126426f1ab89df5
Nishant Mittal <mittalnishant14@outlook.com>
2020-01-18 18:41:31 +0530

    Solve the issue using JS

---
+++ static/js/night_mode.js
@@ -0,0 +1,15 @@
+// The night mode button doesn't appear correctly in firefox
+// So, as a temporary fix the button would be shown only in chrome
+// This file can be deleted after fixing the actual issue
+$(document).ready(function() {
+    var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+    if(!isChrome) {
+        var allToggleButtons = $(".toggle-container");
+        console.log($(".toggle-container").length);
+        var i = 0;
+        while (i < allToggleButtons.length) {
+            allToggleButtons[i].classList.add('hide-toggle-button');
+            i++;
+        }
+    }
+});
