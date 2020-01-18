bd8bfcd0b5586d24d315f2802457e3defa6479db
Nishant Mittal <36989112+nishantwrp@users.noreply.github.com>
2020-01-19 00:54:38 +0530

    Display toggle button only in chrome based browsers (#64)
    Display toggle button only in chrome based browsers

---
+++ static/js/night_mode.js
@@ -0,0 +1,14 @@
+// The night mode button doesn't appear correctly in firefox
+// So, as a temporary fix the button would be shown only in chrome
+// This file can be deleted after fixing the actual issue
+$(document).ready(function() {
+    var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+    if(!isChrome) {
+        var allToggleButtons = $(".toggle-container");
+        var i = 0;
+        while (i < allToggleButtons.length) {
+            allToggleButtons[parseInt(i, 10)].classList.add("hide-toggle-button");
+            i++;
+        }
+    }
+});
